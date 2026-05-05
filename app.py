78386d7970da942f43574bccced0dcdd96e6971f
Troy Savramis <troysavramis@gmail.com>
2026-05-05 16:42:18 -0400

    Updated app.py
    Fixed some mistakes (var names),
    changed the used port from 5000 to 8000 (my computer was already using 5000).

--- app.py
+++ app.py
@@ -2,15 +2,15 @@
 import uvicorn
 from fastapi import FastAPI
 
-from api.routes import tasks_bp
+from api.routes import tasks_router
 
 # app = Flask(__name__)
 app = FastAPI()
 
 # Register the blueprint
 # app.register_blueprint(tasks_bp)
-app.include_router(tasks_bp, prefix="/tasks")
+app.include_router(tasks_router, prefix="/tasks")
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=5000, debug=True)
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, debug=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
